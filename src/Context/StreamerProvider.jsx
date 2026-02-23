655ec0c0ba4d62267b2bd4889d787b5593a2045b
Amir606800 <asadliamir800@gmail.com>
2026-02-23 10:45:17 +0400

    API renew after a very long Time

--- src/Context/StreamerProvider.jsx
+++ src/Context/StreamerProvider.jsx
@@ -9,14 +9,14 @@ export const StreamerProvider = ({ children }) => {
   const [filteredStreamer, setFilteredStream] = useState(streamData);
   useEffect(() => {
     axios
-      .get("https://mocki.io/v1/8b251419-746c-49dd-adae-f3fc6cd984d2")
+      .get("https://mocki.io/v1/404d3833-6f81-471f-8583-3cb69c20cba3")
       .then((res) => setStrmData(res.data.donators))
       .catch((err) => console.log(err));
   }, []);
 
   useEffect(() => {
     const searchResults = streamData.filter((item) =>
-      item.name.toLowerCase().includes(searcInput.toLowerCase())
+      item.name.toLowerCase().includes(searcInput.toLowerCase()),
     );
     setFilteredStream(searchResults);
   }, [streamData, searcInput]);
